3966cca2b5f6f51ec13a548e4ad27433982beecd
David <dave@activelylazy.co.uk>
2011-07-07 21:56:13 +0100

    Changed search results page to use annotation style

--- src/test/java/uk/co/activelylazy/selenium/AmazonSearchResultsPage.java
+++ src/test/java/uk/co/activelylazy/selenium/AmazonSearchResultsPage.java
@@ -1,18 +1,22 @@
 package uk.co.activelylazy.selenium;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class AmazonSearchResultsPage {
 
 	private WebDriver driver;
+	
+	@FindBy(css="#result_0 .title a")
+	private WebElement topResultTitle;
 	
 	public AmazonSearchResultsPage(WebDriver driver) {
 		this.driver = driver;
 	}
 	
 	public String getTopResultTitle() {
-		return driver.findElement(By.cssSelector("#result_0 .title a")).getText();
+		return topResultTitle.getText();
 	}
 
 }
