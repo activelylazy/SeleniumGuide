1050c6321ea228b7ba448838a40461f202f724b1
David <dave@activelylazy.co.uk>
2011-07-07 19:18:40 +0100

    Renamed search and made pass in maven build

--- src/test/java/uk/co/activelylazy/selenium/SimpleAmazonSearchTest.java
+++ src/test/java/uk/co/activelylazy/selenium/SimpleAmazonSearchTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class AmazonSearch {
+public class SimpleAmazonSearchTest {
 
 	private FirefoxDriver driver;
 
